84805a5df22418d6e3fd385d943861ec173adf0b
tushars-optimus <tushar.srivastava@optimusinfo.com>
2017-09-14 17:35:57 +0530

    Completed Assignment-1

---
+++ C#/Assignment-1/Assignment-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class TestIf
+    {
+        private static string _choice;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your preferred language");
+            _choice = Console.ReadLine();
+            if (_choice == "VB")
+            {
+                Console.WriteLine("VB .NET: OOP, multithreading and more!");
+            }
+            else if (_choice == "C#")
+            {
+                Console.WriteLine("Good choice, C# is a fine language.");
+            }
+            else
+            {
+                Console.WriteLine("Well...good luck with that!");
+            }
+            
+        }
+    }
+}
