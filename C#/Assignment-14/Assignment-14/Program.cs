c4cb8d42960464c017634771cdd352ba58b4960b
tushars-optimus <tushar.srivastava@optimusinfo.com>
2017-09-20 18:45:00 +0530

    Assignment-14

---
+++ C#/Assignment-14/Assignment-14/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_14
+{
+    class Program
+    {
+        public static int Count = 1;
+        static void Main(string[] args)
+        {
+            Vehicle vehicle1 = new Vehicle("Bike", "Black",2);
+            Vehicle vehicle2 = new Vehicle("Car", "Green",4);
+            Vehicle vehicle3 = new Vehicle("Truck", "Yellow",6);
+            Vehicle vehicle4 = new Vehicle("Scooter", "Pink",2);
+            Vehicle vehicle5 = new Vehicle("Scooter", "Pink", 2);
+            Vehicle vehicle6 = new Vehicle("Bicyle", "red", 2);
+            List<Vehicle> collection = new List<Vehicle>();
+            collection.Add(vehicle1);
+            collection.Add(vehicle2);
+            collection.Add(vehicle3);
+            collection.Add(vehicle4);
+            collection.Add(vehicle5);
+            collection.Add(vehicle6);
+            Console.WriteLine("Unsorted Vehicle");
+            Console.WriteLine();
+            foreach (var item in collection)
+            {
+                Console.WriteLine(Count + ")" + item);
+                Count++;
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sorted Vehicle");
+            Console.WriteLine();
+            collection.Sort();
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+            Console.WriteLine("checking for Equality between vehicle 1 and vehicle 2");
+            Console.WriteLine(vehicle1.Equals(vehicle2));
+            Console.WriteLine("checking for Equality between vehicle 1 and vehicle 5");
+            Console.WriteLine(vehicle1.Equals(vehicle5));
+            Console.WriteLine("checking for Equality between vehicle 4 and vehicle 5");
+            Console.WriteLine(vehicle5.Equals(vehicle4));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Assignment-14/Assignment-14/Vehicle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_14
+{
+    public class Vehicle:IComparable<Vehicle>,IEquatable<Vehicle>
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Tyres { get; set; }
+        public Vehicle(string name, string color, int tyre)
+        {
+            Name = name;
+            Color = color;
+            Tyres=tyre;
+        }
+
+        // To arrange tyre in Ascending order 
+
+        public int CompareTo(Vehicle other)
+        {
+
+            if (this.Tyres > other.Tyres)
+            {
+                return 1;
+            }
+            else if (this.Tyres < other.Tyres)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
+        ////To compare vehicle Name Alphabeticcaly Ascending
+
+        //public int CompareTo(Vehicle other)
+        //{
+        //    int result = other.Name.CompareTo(this.Name);
+        //     return result;
+        //}
+
+        ////To compare vehicle Name Alphabeticcaly Desending
+        //public int CompareTo(Vehicle other)
+        //{
+        //    int result = this.Name.CompareTo(other.Name);
+        //    return result;
+        //}
+
+        public bool Equals(Vehicle other)
+        {
+            if (this.Name == other.Name)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public override string ToString()
+        {
+            return "Vehicle name is: " + Name + "\nVehicle color is: " + Color + "\nNumber of tyres is: " + Tyres;
+        }
+    }
+}
