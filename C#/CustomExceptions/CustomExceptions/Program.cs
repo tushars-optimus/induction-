0aa94860af37db45b8eaac110005777a4fb86b56
tushars-optimus <tushar.srivastava@optimusinfo.com>
2017-10-05 18:58:58 +0530

    Assignment-16

---
+++ C#/CustomExceptions/CustomExceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Vehicles vehicle1 = new Vehicles() { Name = "Car", Speed = 80 };
+            Vehicles vehicle2 = new Vehicles() { Name = "Bicycle", Speed = 40 };
+            for (int i = 30; i < 150;i=i+30)
+            {
+                vehicle1.Accelerate(i);
+                vehicle1.Deaccelerate(10);
+                vehicle1.check();
+                vehicle2.Accelerate(i);
+                vehicle2.Deaccelerate(20);
+                vehicle2.check();
+            }
+                Console.ReadLine();
+
+        }
+    }
+}
---
+++ C#/CustomExceptions/CustomExceptions/Vehicles.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions
+{
+    public class Vehicles
+    {
+        const int CarMax = 220;
+        const int BicycleMax = 100;
+        const int CarExplode = 280;
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+    
+    private int _speed;
+
+	public int Speed
+	{
+		get { return _speed;}
+		set { _speed = value;}
+	}
+        public int Accelerate(int speed)
+    {
+        Speed = Speed + speed;
+        return Speed;
+    }
+        public int Deaccelerate(int speed)
+        {
+            Speed = Speed - speed;
+            return Speed;
+        }
+
+        public void check()
+        {
+            try
+            {
+                if (Name == "Car")
+                {
+                    Console.WriteLine(" Car speed is " + Speed);
+                }
+                if (Name == "Bicycle")
+                {
+                    Console.WriteLine(" Bicycle speed is " + Speed);
+                }
+                if (Speed > CarMax && Name == "Car" && Speed < CarExplode)
+                {
+                    throw new IsCarDeadException("Car has overheated");
+                }
+                if (Speed > BicycleMax && Name == "Bicycle")
+                {
+                    throw new IsCarDeadException("Bicycle can not go above 100 km/hr");
+                }
+                if (Speed > CarExplode && Name == "Car")
+                {
+                    
+                    throw new IsCarDeadException("Car exploded");
+                }
+            }
+            catch (IsCarDeadException e)
+            {
+                string Time = DateTime.Now.ToShortTimeString();
+                Console.WriteLine("Exception thrown: " + " Speed: " + Speed +" "+e.Message + " Time is " + Time);
+            }
+            
+            
+        }
+  }
+}
