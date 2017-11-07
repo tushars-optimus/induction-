5c2a249aea5bf0040ae6d3eb839889b2d1d67bd0
tushars-optimus <tushar.srivastava@optimusinfo.com>
2017-11-07 21:13:04 +0530

    Assignment_21

---
+++ C#/Assignment_21/Assignment_21/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using CsvHelper;
+
+namespace Assignment_21
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "csv file (*.csv)|*.csv|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = @"C:\Users\tushar.srivastava\Documents\visual studio 2013\Projects\Assignment_21\Assignment_21\Assignment-21(a)";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    Content.Text=Path.GetFileName(filename);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string path = @"C:\Users\tushar.srivastava\Documents\visual studio 2013\Projects\Assignment_21\Assignment_21\Assignment-21(a)";
+            String originalPath=string.Format(path+"\\"+Content.Text);
+            var file = new StreamReader(originalPath); 
+            var reader = new CsvReader(file);
+
+            //CSVReader will now read the whole file into an enumerable
+            IEnumerable<Student> records = reader.GetRecords<Student>();
+            var studentinfo = new StudentDatabaseContext();
+            foreach (Student record in records)
+            {
+                StudentDb student = new StudentDb() { Name = record.Name, Id = record.Id, Address = record.Address };
+                studentinfo.StudentDbs.Add(student);
+
+                studentinfo.SaveChanges();
+                
+            }
+            
+
+        }
+    }
+}
---
+++ C#/Assignment_21/Assignment_21/StudentDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_21
+{
+    public class StudentDatabaseContext:DbContext
+    {
+        public StudentDatabaseContext():base("name=Assignment")
+        {
+
+        }
+        public  DbSet<StudentDb> StudentDbs { get; set; }
+    }
+}
